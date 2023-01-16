bc7883188b8c796e038a8bd2e2c1c2f16b9c4707
sayyidmaulanafathir <111265341+sayyidmaulanafathir@users.noreply.github.com>
2023-01-16 09:54:25 +0700

    Add files via upload

---
+++ logika.js
@@ -0,0 +1,36 @@
+let a = true;
+let b = false;
+let c = true;
+let d = false;
+
+//or
+
+//akan bernilai true jika salah satu true
+console.log(d || b || a);
+//and
+//akan bernilai false jika salah satu false
+console.log(a && b && c );
+
+let x =8;
+let y =7;
+
+//lebih besar
+console.log(x > y);
+
+//lebih kecil
+console.log(x < y);
+
+//sama dengan
+console.log(x == y);
+
+//lebih besar sama dengan
+console.log(x >= y);
+
+//lebih kecil sama dengan
+console.log(x <= y);
+
+let lima = "5";
+let limo = 5;
+
+//identik
+console.log(lima === limo);
---
+++ loop.js
@@ -0,0 +1,31 @@
+let mulai = 0;
+let selesai = 10;
+
+while (mulai <= selesai) {
+    console.log(mulai);
+
+    mulai++;
+}
+
+let start = 101;
+let finish =110;
+
+do {
+    console.log(start);
+    start++;
+}while (start < finish);
+
+let awal =1;
+let akhir =10;
+
+for (awal ; awal < akhir; awal++ ) {
+    console.log(awal);
+
+}
+
+console.log("--------------");
+
+for (let i =0; i < 10; i = i + 4) {
+    console.log(i);
+    
+}
