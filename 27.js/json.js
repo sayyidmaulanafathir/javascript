5270c2e7412d85a302d2794a2cb74a73bd1d44ae
sayyidmaulanafathir <111265341+sayyidmaulanafathir@users.noreply.github.com>
2023-02-13 09:53:24 +0700

    Add files via upload

---
+++ 27.js/json.js
@@ -0,0 +1,23 @@
+let tblmenu = [ {
+    "idmenu" : 1,
+    "idkategori" : 1,
+    "menu" : "apel manalagi",
+    "harga" : "3000",
+    "warna" : ["merah" , "kuning" , "hijau"],
+    "stok" : true,
+    "keterangan" : null
+},
+
+{
+    "idmenu" : 2,
+    "idkategori" : 1,
+    "menu" : "pisang",
+    "harga" : "2000",
+    "warna" : ["merah" , "kuning"],
+    "stok" : false,
+    "keterangan" : null
+}
+
+]
+
+console.log(tblmenu[0].menu);
---
+++ 28.js/json.js
@@ -0,0 +1,32 @@
+document.querySelector("klik").addEventListener("click",tampil);
+
+function tampil(params) {
+    console.log("tes");
+}
+
+
+
+
+// let tblmenu = [ {
+//     "idmenu" : 1,
+//     "idkategori" : 1,
+//     "menu" : "apel manalagi",
+//     "harga" : "3000",
+//     "warna" : ["merah" , "kuning" , "hijau"],
+//     "stok" : true,
+//     "keterangan" : null
+// },
+
+// {
+//     "idmenu" : 2,
+//     "idkategori" : 1,
+//     "menu" : "pisang",
+//     "harga" : "2000",
+//     "warna" : ["merah" , "kuning"],
+//     "stok" : false,
+//     "keterangan" : null
+// }
+
+// ]
+
+// console.log(tblmenu[0].menu);
